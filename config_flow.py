05492391422172b39ff2084072a2ec70b2ae2e2d
Mark Coombes <mark@markcoombes.ca>
2021-01-30 00:26:06 -0500

    Enhance config flow

--- config_flow.py
+++ config_flow.py
@@ -1,4 +1,5 @@
 import logging
+from pyhs3 import HomeTroller, DEVICE_ZWAVE_SWITCH_MULTILEVEL
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -7,6 +8,8 @@ from homeassistant.const import (
     CONF_PASSWORD,
     CONF_USERNAME,
 )
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import homeassistant.helpers.config_validation as cv
 
 from .const import (
     CONF_ALLOW_EVENTS,
@@ -30,18 +33,21 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-ENTRY_SCHEMA = vol.Schema(
+USER_STEP_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_HOST): str,
-        vol.Required(CONF_NAMESPACE, default=DEFAULT_NAMESPACE): str,
-        vol.Required(CONF_PASSWORD, default=DEFAULT_PASSWORD): str,
-        vol.Required(CONF_USERNAME, default=DEFAULT_USERNAME): str,
-        vol.Required(CONF_HTTP_PORT, default=DEFAULT_HTTP_PORT): int,
-        vol.Required(CONF_ASCII_PORT, default=DEFAULT_ASCII_PORT): int,
-        vol.Required(CONF_NAME_TEMPLATE, default=DEFAULT_NAME_TEMPLATE): str,
-        vol.Required(CONF_ALLOW_EVENTS, default=DEFAULT_ALLOW_EVENTS): bool,
-        vol.Optional(CONF_ALLOWED_EVENT_GROUPS): str,
-        vol.Optional(CONF_FORCED_COVERS): str,
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_USERNAME, default=DEFAULT_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD, default=DEFAULT_PASSWORD): cv.string,
+        vol.Required(CONF_HTTP_PORT, default=DEFAULT_HTTP_PORT): cv.port,
+        vol.Required(CONF_ASCII_PORT, default=DEFAULT_ASCII_PORT): cv.port,
+    }
+)
+
+CONFIG_STEP_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAMESPACE, default=DEFAULT_NAMESPACE): cv.string,
+        vol.Required(CONF_NAME_TEMPLATE, default=DEFAULT_NAME_TEMPLATE): cv.string,
+        vol.Required(CONF_ALLOW_EVENTS, default=DEFAULT_ALLOW_EVENTS): cv.boolean,
     }
 )
 
@@ -52,6 +58,21 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
+    def __init__(self):
+        self._homeseer = None
+        self._host = None
+        self._username = None
+        self._password = None
+        self._http_port = None
+        self._ascii_port = None
+        self._namespace = None
+        self._name_template = None
+        self._allow_events = None
+        self._switch_multilevels = []
+        self._event_groups = []
+        self._forced_covers = []
+        self._allowed_groups = []
+
     async def async_step_user(self, user_input=None):
         errors = {}
         if self._async_current_entries():
@@ -59,12 +80,104 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is not None:
-
-            return self.async_create_entry(
-                title=user_input[CONF_HOST],
-                data=user_input,
+            self._homeseer = HomeTroller(
+                user_input[CONF_HOST],
+                async_get_clientsession(self.hass),
+                user_input[CONF_USERNAME],
+                user_input[CONF_PASSWORD],
+                user_input[CONF_HTTP_PORT],
+                user_input[CONF_ASCII_PORT],
             )
+            try:
+                await self._homeseer.initialize()
+            except:
+                errors["base"] = "initialize_failed"
+
+            if len(self._homeseer.devices) > 0 or len(self._homeseer.events) > 0:
+                self._host = user_input[CONF_HOST]
+                self._username = user_input[CONF_USERNAME]
+                self._password = user_input[CONF_PASSWORD]
+                self._http_port = user_input[CONF_HTTP_PORT]
+                self._ascii_port = user_input[CONF_ASCII_PORT]
+
+                for device in self._homeseer.devices:
+                    if device.device_type_string == DEVICE_ZWAVE_SWITCH_MULTILEVEL:
+                        self._switch_multilevels.append(f"{device.ref} - {device.name}")
+                for event in self._homeseer.events:
+                    if event.group not in self._event_groups:
+                        self._event_groups.append(event.group)
+                return await self.async_step_config()
 
         return self.async_show_form(
-            step_id="user", data_schema=ENTRY_SCHEMA, errors=errors
+            step_id="user", data_schema=USER_STEP_SCHEMA, errors=errors
+        )
+
+    async def async_step_config(self, user_input=None):
+
+        if user_input is not None:
+            self._namespace = user_input[CONF_NAMESPACE]
+            self._name_template = user_input[CONF_NAME_TEMPLATE]
+            self._allow_events = user_input[CONF_ALLOW_EVENTS]
+
+            if len(self._switch_multilevels) > 0:
+                return await self.async_step_multilevels()
+            if len(self._event_groups) > 0 and self._allow_events:
+                return await self.async_step_groups()
+            return self.finalize_entry_flow()
+
+        return self.async_show_form(step_id="config", data_schema=CONFIG_STEP_SCHEMA)
+
+    async def async_step_multilevels(self, user_input=None):
+
+        if user_input is not None:
+            self._forced_covers = user_input[CONF_FORCED_COVERS]
+
+            if len(self._event_groups) > 0 and self._allow_events:
+                return await self.async_step_groups()
+            return self.finalize_entry_flow()
+
+        return self.async_show_form(
+            step_id="multilevels",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_FORCED_COVERS): cv.multi_select(
+                        self._switch_multilevels
+                    )
+                }
+            ),
+        )
+
+    async def async_step_groups(self, user_input=None):
+
+        if user_input is not None:
+            self._allowed_groups = user_input[CONF_ALLOWED_EVENT_GROUPS]
+
+            return self.finalize_entry_flow()
+
+        return self.async_show_form(
+            step_id="groups",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_ALLOWED_EVENT_GROUPS): cv.multi_select(
+                        self._event_groups
+                    )
+                }
+            ),
+        )
+
+    def finalize_entry_flow(self):
+        return self.async_create_entry(
+            title=self._host,
+            data={
+                CONF_HOST: self._host,
+                CONF_USERNAME: self._username,
+                CONF_PASSWORD: self._password,
+                CONF_HTTP_PORT: self._http_port,
+                CONF_ASCII_PORT: self._ascii_port,
+                CONF_NAMESPACE: self._namespace,
+                CONF_NAME_TEMPLATE: self._name_template,
+                CONF_ALLOW_EVENTS: self._allow_events,
+                CONF_FORCED_COVERS: self._forced_covers,
+                CONF_ALLOWED_EVENT_GROUPS: self._allowed_groups,
+            },
         )
